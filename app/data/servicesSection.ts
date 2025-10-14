17584992146511fe6ef3cc8ce9ab461e24685c52
ahmadansari <gyratedigital@gmail.com>
2025-10-14 18:02:01 +0500

    Services and contact page is ready

--- app/data/servicesSection.ts
+++ app/data/servicesSection.ts
@@ -4,25 +4,33 @@ export const servicesSection = [
     {
         id: 1,
         number: "01",
+        category: "UI/UX Design",
         title: ["Custom Web", " Solutions?"],
         description: "We create tailored web solutions designed to fit your unique business needs. From responsive websites to advanced web applications, our services ensure performance, scalability, and a seamless user",
+        image: "/work-1.png",
     },
     {
         id: 2,
         number: "02",
+        category: "Frontend Development",
         title: ["Innovative Mobile", " Applications?"],
         description: "We design and develop cutting-edge mobile applications that combine creativity with functionality. Our apps deliver seamless performance, user-friendly interfaces, and innovative features to engage your audience",
+        image: "/work-2.png",
     },
     {
         id: 3,
         number: "03",
+        category: "Backend Development",
         title: ["Digital Marketing", " Strategies?"],
         description: "We craft data-driven digital marketing strategies that boost your online presence, engage your target audience, and drive measurable business growth.",
+        image: "/work-3.png",
     },
     {
         id: 4,
         number: "04",
+        category: "Full Stack Development",
         title: ["Search Engine", " Optimization?"],
         description: "We optimize your website with proven SEO techniques to improve visibility, rank higher on search engines, and attract the right audience to grow your business organically.",
+        image: "/work-4.png",
     },
 ];
