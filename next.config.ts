29982d84c8ddd6efd8719dfdbb9f0dbadb5bf8ee
dev <gyratedigital@gmail.com>
2025-12-29 17:58:05 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // @ts-expect-error - 'turbopack' is a top-level property and requires an absolute path in Next.js 16
   turbopack: {
     root: process.cwd(),
   },
-  // @ts-expect-error - 'allowedDevOrigins' has moved to the top level in Next.js 16
   allowedDevOrigins: ["192.168.1.3"],
 };
 
