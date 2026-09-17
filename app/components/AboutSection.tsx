d069a6c90a4f8a5d95a0eac68a68b057f6c2b3dd
Gyrate Digital <gyratedigital@gmail.com>
2026-09-17 16:39:48 +0500

    Update AboutSection.tsx

--- app/components/AboutSection.tsx
+++ app/components/AboutSection.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import Link from 'next/link';
 import { useRippleEffect } from "@/hooks/useRippleEffect";
 
+// test 
 export default function AboutSection() {
     const { ripples, addRipple } = useRippleEffect();
 
